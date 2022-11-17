aca0367e077edcdbe4741aee7404e7d95d40fbeb
Paul Fletes <paul.fletes@gmail.com>
2022-11-16 20:29:36 -0600

    make game objects move

--- example2.py
+++ example2.py
@@ -1,6 +1,11 @@
 # import / initialize pygame
+from random import randint
 import pygame
 pygame.init()
+
+
+# get the clock
+clock = pygame.time.Clock()
 
 # configure screen size
 screen = pygame.display.set_mode([500, 500])  # this call returns screen obj
@@ -19,10 +24,33 @@ class GameObject(pygame.sprite.Sprite):
     def render(self, screen):
         screen.bilt(self.surf, (self.x, self.y))
 
+# class extending game object
+
+
+class Apple(GameObject):
+    def __init__(self):
+        super(Apple, self).__init__(0, 0, 'apple.png')
+        self.dx = 0
+        self.dy = (randint(0, 200) / 100) + 1  # sets speed of apple's mvmt
+        self.reset()  # call restart method
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        # check y position of apple
+        if self.y > 500:
+            self.reset()
+
+    # add a new method to get apple back to top of screen
+    def reset(self):
+        self.x = randint(50, 400)
+        self.y = -64
+
 
 # make instance of game object
 # box = GameObject(120, 300, 50, 50)
-apple = GameObject(120, 300, 'apple.png')
+# apple = GameObject(120, 300, 'apple.png')
+apple = Apple()
 
 # make instance of strawberry game object
 strawberry = GameObject(400, 100, 'strawberry.png')
@@ -48,6 +76,7 @@ while running:
     # box.render(screen)
 
     # draw apple
+    apple.move()
     apple.render(screen)
 
     # challenge 1: draw a strawberry
@@ -55,3 +84,6 @@ while running:
 
     # update display
     pygame.display.flip()
+
+    # tick the clock!
+    clock.tick(60)  # updates applied in 1/30th of a sec.
